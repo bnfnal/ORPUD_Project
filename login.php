52505353135fd45126064ccdd3ecff0da96af15e
bnfnal <bnfnal@gmail.com>
2023-04-25 18:27:32 +0300

    2

--- login.php
+++ login.php
@@ -1,4 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Вход</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="form">
+        <h1>Вход</h1>
+        <form action="main.php" method="post">
+            <div class="input-form">
+                <input type="text" placeholder="email" name="email">
+            </div>
+            <div class="input-form">
+                <input type="text" placeholder="password" name="pass">
+            </div>
+            <div class="input-form">
+                <input type="submit" value="Войти">
+            </div>
+        </form>
 
-$login = $_POST['login'];
-$pass = $_POST['pass'];
+        <form action="forgot_passwd.php">
+            <div class="forget">
+                <input type="submit" value="Забыли пароль?">
+            </div>
+        </form>
+
+        <form action="register.php">
+            <div class="input-form">
+                <input type="submit" value="Зарегистрироваться">
+            </div>
+        </form>
+
+    </div>
+
+</body>
+</html>
+
+<?php
--- register.php
+++ register.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Регистрация</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<form action="login.php" method="post">
+    <p>ФИО: <input type="text" placeholder="name" name="name"> </p>
+    <p>Дата рождения: <input type="text" placeholder="date" name="date"> </p>
+    <p>email: <input type="text" placeholder="email" name="email"> </p>
+    <p>Пароль:  <input type="text" placeholder="password" name="pass"> </p>
+    <p>Повторите пароль: <input type="text" placeholder="repeat password" name="repeatpass"> </p>
+    <p> <button type="submit">Зарегистрироваться</button>
+</form>
+
+
+</body>
+</html>
+
+<?php
+
+?>
 <?php
 
 $login = $_POST['login'];
